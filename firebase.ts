060f8b4543b10a57697730706d831e2256a0366f
= <=>
2025-05-07 01:31:47 -0700

    Secure App

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSPERxHmhqJez1cmXRDTM_mt1tfy8QBHE",
-  authDomain: "fire-api-b166b.firebaseapp.com",
-  projectId: "fire-api-b166b",
-  storageBucket: "fire-api-b166b.appspot.com",
-  messagingSenderId: "519060328603",
-  appId: "1:519060328603:web:0d1f82bec9a4b98f26363e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
