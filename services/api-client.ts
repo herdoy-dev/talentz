5e93a563dcca84e675397a4cebf7d8d24fac54f3
= <=>
2025-06-06 01:37:35 +0600

    Update ApiClient

--- services/api-client.ts
+++ services/api-client.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  withCredentials: true,
 });
 
 instance.interceptors.request.use(async (config) => {
